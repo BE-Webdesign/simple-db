abfaf8ef8108794ee88f29cb59f6e7602aa4c661
BE-Webdesign <cromleyedwin89@gmail.com>
2022-07-30 18:24:13 -0400

    Add lexing function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::iter::Peekable;
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -35,6 +37,45 @@ pub enum LexItem {
     Paren(char),
     Op(char),
     Num(u64),
+}
+
+fn lex(input: &String) -> Result<Vec<LexItem>, String> {
+    let mut result = Vec::new();
+
+    let mut it = input.chars().peekable();
+    while let Some(&c) = it.peek() {
+        match c {
+            '0'..='9' => {
+                it.next();
+                let n = get_number(c, &mut it);
+                result.push(LexItem::Num(n));
+            }
+            '+' | '*' => {
+                result.push(LexItem::Op(c));
+                it.next();
+            }
+            '(' | ')' | '[' | ']' | '{' | '}' => {
+                result.push(LexItem::Paren(c));
+                it.next();
+            }
+            ' ' => {
+                it.next();
+            }
+            _ => {
+                return Err(format!("unexpected character {}", c));
+            }
+        }
+    }
+    Ok(result)
+}
+
+fn get_number<T: Iterator<Item = char>>(c: char, iter: &mut Peekable<T>) -> u64 {
+    let mut number = c.to_string().parse::<u64>().expect("The caller should have passed a digit.");
+    while let Some(Ok(digit)) = iter.peek().map(|c| c.to_string().parse::<u64>()) {
+        number = number * 10 + digit;
+        iter.next();
+    }
+    number
 }
 
 pub const GREETING: &'static str = "Hallo, Rust library here!\n";
